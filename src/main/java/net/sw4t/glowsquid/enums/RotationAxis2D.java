5e8dcf35b172f0037b5421e3e8afb92472ba6291
AppleDash <pet@feather.horse>
2021-04-08 05:12:05 -0400

    Add RotationAxis

---
+++ src/main/java/net/sw4t/glowsquid/enums/RotationAxis2D.java
@@ -0,0 +1,8 @@
+package net.sw4t.glowsquid.enums;
+
+/**
+ * Represents an axis of rotation, with two possible choices (like a video game character.)
+ */
+public enum RotationAxis2D {
+    YAW, PITCH
+}
---
+++ src/main/java/net/sw4t/glowsquid/enums/RotationAxis3D.java
@@ -0,0 +1,8 @@
+package net.sw4t.glowsquid.enums;
+
+/**
+ * Represents an axis of rotation, with three possible choices (like an airplane.)
+ */
+public enum RotationAxis3D {
+    YAW, PITCH, ROLL
+}
